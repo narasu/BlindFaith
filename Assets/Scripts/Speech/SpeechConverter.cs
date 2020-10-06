ddcc6da1e9797da34b45954ba62656760de2943c
Michael <roninnarasu@gmail.com>
2020-10-06 17:45:31 +0200

    added comments/pseudocode

--- Assets/Scripts/Speech/SpeechConverter.cs
+++ Assets/Scripts/Speech/SpeechConverter.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SpeechConverter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //some function that outputs voice input as string
 }
