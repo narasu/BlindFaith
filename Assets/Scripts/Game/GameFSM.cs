2790bb11c43848a9594b20b65394332a9d270cf7
Michael <roninnarasu@gmail.com>
2020-10-07 14:20:51 +0200

    working FSM, more pseudocode

--- Assets/Scripts/Game/GameFSM.cs
+++ Assets/Scripts/Game/GameFSM.cs
@@ -2,17 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameFSM : MonoBehaviour
+public class GameFSM
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameManager owner;
+
+    private Dictionary<GameStateType, GameState> states;
+
+    public GameStateType CurrentStateType { get; private set; }
+    public GameStateType PreviousStateType { get; private set; }
+
+    private GameState currentState;
+    private GameState previousState;
+
+    
+
+    public void Initialize(GameManager _owner)
     {
-        
+        owner = _owner;
+
+        states = new Dictionary<GameStateType, GameState>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddState(GameStateType newType, GameState newState)
     {
-        
+        states.Add(newType, newState);
+        states[newType].Initialize(this);
+    }
+
+    public void UpdateState()
+    {
+        currentState?.Update();
+    }
+
+    public void GotoState(GameStateType key)
+    {
+        if (!states.ContainsKey(key))
+        {
+            return;
+        }
+
+        currentState?.Exit();
+
+        previousState = currentState;
+        PreviousStateType = CurrentStateType;
+
+        CurrentStateType = key;
+        currentState = states[CurrentStateType];
+
+        currentState.Enter();
+    }
+
+    public GameState GetState(GameStateType type)
+    {
+        if (!states.ContainsKey(type))
+        {
+            return null;
+        }
+        return states[type];
     }
 }
