f4315c3a81deddb75c5f045074a57cc66a7265af
RoryPumpkin <rowinvanderschee@gmail.com>
2020-10-14 22:48:20 +0200

    Added mainmenu scene
    In mainmenu is een MenuManager die script naam als public string wilt voor de scene waar je naartoe wilt switchen als de speler op spatiebalk drukt. Beide sounds in de scene staan op loop.

---
+++ Assets/Scripts/Game/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public string sceneToSwapTo;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneToSwapTo);
+        }
+    }
+}
